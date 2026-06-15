cde34df1d1bcde8fd5db20a29ca62581163343e3
listermatt-sudo <lister.matt@googlemail.com>
2026-06-15 12:00:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 
 app = FastAPI()
 
-SUPABASE_URL = "https://your-project-id.supabase.co"
-SUPABASE_KEY = "your-anon-key"
+SUPABASE_URL = "https://gdcwjpkgffqmatsmuqra.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkY3dqcGtnZmZxbWF0c211cXJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4ODMxMzksImV4cCI6MjA5NjQ1OTEzOX0.bpjgWe1ydbiIK2e9yOwESvMRLoK5c_lHljCpOM8q-3o"
 
 
 @app.get("/")
